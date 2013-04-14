70052f4aa9f62db054894e4a8b29c35e45d494fe
nan0tube <nan0tube@yahoo.com>
2013-04-14 15:15:52 +0800

    Allow post-hook without argument

--- hook_once.py
+++ hook_once.py
@@ -1,17 +1,20 @@
-from functools import wraps
+import functools
+import inspect
 
 def pre_hook_once(original):
     """
     Decorator to insert a pre-hook in original function with target function.
     The hook is removed after first execution.
 
-    Arguments can be passed to original function by returning the tuple
+    Arguments can be passed to original function by returning a tuple
     (args, kwargs). Returning None will not modify any argument.
     """
     def decorator(target):
-        @wraps(target)
+        @functools.wraps(target)
         def wrapper(*args, **kwargs):
-            target.func_globals[target.__name__] = original
+            if target.func_name not in target.func_globals:
+                raise NameError('Function not in global scope')
+            target.func_globals[target.func_name] = original
             ret = target(*args, **kwargs)
             if ret:
                 (args, kwargs) = ret
@@ -24,14 +27,21 @@ def post_hook_once(original):
     Decorator to insert a post-hook in original function with target function.
     The hook is removed after first execution.
 
-    Return value is passed to target function as kwargs['target_retval'].
+    Return value of original function is passed to target function as
+    target_retval (keyword argument). Return value is not passed if target
+    function has no argument.
     """
     def decorator(target):
-        @wraps(target)
+        @functools.wraps(target)
         def wrapper(*args, **kwargs):
-            target.func_globals[target.__name__] = original
+            if target.func_name not in target.func_globals:
+                raise NameError('Function not in global scope')
+            target.func_globals[target.func_name] = original
             kwargs['target_retval'] = original(*args, **kwargs)
-            return target(*args, **kwargs)
+            args, varargs, keywords, defaults = inspect.getargspec(target)
+            if args or varargs or keywords:
+                return target(*args, **kwargs)
+            return target()
         return wrapper
     return decorator
 
@@ -42,7 +52,7 @@ if __name__ == '__main__':
 
     @pre_hook_once(pprint)
     def pprint(*args, **kwargs):
-        return [args[0].swapcase()], kwargs
+        return ([args[0].swapcase()], kwargs)
 
     print pprint('world')
     print pprint('world')
